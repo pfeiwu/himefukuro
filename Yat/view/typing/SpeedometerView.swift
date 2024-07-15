f0e21ccaa5db59c130898287b623bc557d5745c8
Pengfei Wu <dev.pengfei.wu@gmail.com>
2024-07-15 13:40:58 +0800

    一个进度条

--- Yat/view/typing/SpeedometerView.swift
+++ Yat/view/typing/SpeedometerView.swift
@@ -16,6 +16,43 @@ struct SpeedometerView: View {
     
     @Environment(AttributeContainer.self) private var attributeCon: AttributeContainer
     
+    private var progress: Double {
+        Double(currentRecord.realInput.count) / Double(currentArticleCon.article.content.count)
+    }
+    
+    private var barColor: Color {
+        let maxCPM: Double = 250
+        let cpmPercentage = min(currentRecord.characterPerMin / maxCPM, 1.0)
+        
+        // Define custom gradient colors: Yellow -> Green -> Blue -> Purple
+        let gradientColors: [(red: Double, green: Double, blue: Double)] = [
+            (red: 1.0, green: 1.0, blue: 0.0),   // Yellow
+            (red: 0.0, green: 1.0, blue: 0.0),   // Green
+            (red: 0.0, green: 0.0, blue: 1.0),   // Blue
+            (red: 0.5, green: 0.0, blue: 0.5)    // Purple
+        ]
+        
+        // Calculate interpolation factor
+        let index = cpmPercentage * Double(gradientColors.count - 1)
+        let startIndex = Int(index)
+        let endIndex = min(startIndex + 1, gradientColors.count - 1)
+        let fraction = index - Double(startIndex)
+        
+        // Perform linear interpolation between colors
+        let startColor = gradientColors[startIndex]
+        let endColor = gradientColors[endIndex]
+        
+        let red = startColor.red + fraction * (endColor.red - startColor.red)
+        let green = startColor.green + fraction * (endColor.green - startColor.green)
+        let blue = startColor.blue + fraction * (endColor.blue - startColor.blue)
+        
+        let interpolatedColor = Color(red: red, green: green, blue: blue)
+        
+        return interpolatedColor
+    }
+
+
+    
     private var currentArticle: Article {
         currentArticleCon.article
     }
@@ -23,15 +60,24 @@ struct SpeedometerView: View {
     // timer by 100ms
     @State private var timer: Timer?
     var body: some View {
-        VStack {
-            Text("速度：\(currentRecord.characterPerMin)")
-        }
-        .onAppear(perform: {
-            self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { _ in
-                if (attributeCon.state == .typing){
-                    currentRecord.timeConsumedInSec += 0.1
-                }
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .fill(barColor)
+                    .frame(width: geometry.size.width * CGFloat(progress), height: 6)
+                    .cornerRadius(3)
+                    .animation(.easeInOut(duration: 0.5), value: progress)
+            }
+            .frame(height: 6)
+            .onAppear(perform: {
+                self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { _ in
+                    if (attributeCon.state == .typing){
+                        currentRecord.timeConsumedInSec += 0.1
+                    }
+                })
             })
-        })
+            .padding()
+        }
+        
     }
 }
